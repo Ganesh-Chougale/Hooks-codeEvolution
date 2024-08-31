26e30809abc61d27b14d988cb16b941c466c36b0
Ganesh-Chougale <gchougale32@gmail.com>
2024-08-31 18:15:01 +0530

    another push

--- hooks-code-evolution/src/App.js
+++ hooks-code-evolution/src/App.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './App.css';
-import Counter01 from './Hooks/RH_useReducer/ExtraExamples/ExtraExample01/Counter03';
+import CompA from './Hooks/RH_Recude_Context/Example01/CompA';
+import CompB from './Hooks/RH_Recude_Context/Example01/CompB';
+import CompC from './Hooks/RH_Recude_Context/Example01/CompC';
 
+export const COUNT_CONTEXT_APP = React.createContext();
 
+const initialVAl = 0;
+const reducer = (state, action)=>{
+  switch(action){
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialVAl;
+    default:
+      return state;  
+  }
+}
 
 function App() {
 
+  const [count, dispatch] = useReducer(reducer, initialVAl);
 
   return (
+
+    <COUNT_CONTEXT_APP.Provider value={{countState: count, countDispatch: dispatch}}>
     <div className="App">
-    <Counter01/>
+    <h1 style={{width: "100%"}}>count {count}</h1>
+    <CompA/>
+    <CompB/>
+    <CompC/>
     </div>
+    </COUNT_CONTEXT_APP.Provider>
+
   );
 }
 
